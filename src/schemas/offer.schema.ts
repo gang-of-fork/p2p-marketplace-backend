f81a5123fc241372316d56758c50e2092533cd92
stat1c <fynn.weyrich@web.de>
2022-09-26 23:06:59 +0200

    progress offer routes

--- src/schemas/offer.schema.ts
+++ src/schemas/offer.schema.ts
@@ -2,7 +2,7 @@ import { Schema } from "https://deno.land/x/valivar@v6.2.11/mod.ts"
 import { Currencies, CryptoCurrencies, OfferTypes } from '../types/offer.ts';
 
 const offerSchema = new Schema({
-    location: [{type: Number, required: true, length: 2}],
+    location: [{type: Number, required: true }],
     type: {type: String, required: true, enum: Object.values(OfferTypes) },
     crypto: {type: String, required: true, enum: Object.values(CryptoCurrencies) },
     currency: {type: String, required: true, enum: Object.values(Currencies) },
