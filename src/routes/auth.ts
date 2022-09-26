22f3ac714c25379ba60b21b12805c1996f5fcfa2
lumaghg <rotkehlchen567@gmail.com>
2022-09-26 21:12:47 +0200

    added more comments

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -25,13 +25,18 @@ import Key from "../collections/keysCollection.ts";
 
 
 export default class AuthController {
+    /**
+     * POST /auth/register
+     */
     static async registerUser(req: OpineRequest, res: OpineResponse, next: NextFunction) {
         const { publicAddress } = req.body;
 
+        //nonceId will be updated in AuthController.updateNonce()
         const userToInsert = {
             publicAddress: publicAddress,
             nonceId: "initial"
         }
+        //insert a user and generate a nonce
         try {
             userSchema.assert(userToInsert)
             await User.insertOne(userToInsert)
@@ -47,6 +52,9 @@ export default class AuthController {
         }
     }
 
+    /**
+     * GET /auth/login/:publicAddress
+     */
     static async getUserForLogin(req: OpineRequest, res: OpineResponse, next: NextFunction) {
         try {
             const nonce = await AuthController.getNonce(req.params.publicAddress);
@@ -59,10 +67,13 @@ export default class AuthController {
             return next({ statusCode: 500, msg: e.toString() });
         }
     }
-
+/**
+ * POST /auth/login
+ */
     static async postLogin(req: OpineRequest, res: OpineResponse, next: NextFunction) {
         const { publicAddress, signature }: { publicAddress: string, signature: string } = req.body;
         try {
+            //check signature format
             if (signature.length != 132 && !signature.startsWith("0x")) {
                 return next({ statusCode: 400, msg: "Invalid signature length or signature does not start with 0x" })
             }
@@ -71,19 +82,19 @@ export default class AuthController {
             if (!user) {
                 return next({ statusCode: 400, msg: "User not Found" })
             }
-
+            
+            //construct verification message
             const nonce = AuthController.getNonce(publicAddress)
-
             const msg = `I am signing my one-time nonce: ${nonce}`
-            //const msg = 'Example `personal_sign` message';
-
             const msgBufferHex = bufferToHex(Buffer.from(msg, 'utf8'))
 
+            //verify that the origin of the signature is the publicAddress from the request
             const address = recoverPersonalSignature({ data: msgBufferHex, sig: signature })
             if (address.toLowerCase() != publicAddress.toLowerCase()) {
                 return next({ statusCode: 401, msg: "Signature Verification failed" })
             }
 
+            //create JWT
             const jwt = await create({ alg: "HS512", typ: "JWT" }, {
                 userId: user._id,
                 publicAddress: user.publicAddress
@@ -97,12 +108,43 @@ export default class AuthController {
 
         finally {
             try {
+                //update nonce after every login attempt
                 AuthController.updateNonce(publicAddress)
-            } catch (_e) {//do nothing
+            } catch (_e) {//
             }
         }
     }
+    
+    /**
+     * retrieve a nonce for a User or update and retrieve it, if it was deleted by TTL index
+     */
+    static async getNonce(publicAddress: string) {
+        //Query user with joined nonce
+        const users = await User.aggregate<TUserLookup>([{ $match: { publicAddress: publicAddress } }, { $lookup: { from: 'nonces', localField: 'nonceId', foreignField: '_id', as: 'nonces' } }]).toArray();
+        if (!users[0]) throw "User not found"
+        const user = users[0];
 
+        //if the nonce still exists, decrypt and return it, if not generate a new Nonce with updateNonce()
+        let nonce: string;
+        if (user.nonces.length != 0) {
+            const encryptedNonce = user.nonces[0].value
+            
+            const iv = await IV.findOne({ _id: user.nonces[0].ivId })
+            if (!iv) throw "IV not found";
+            
+            const key = await Key.findOne({})
+            if(!key) throw "AES Secret missing";
+            
+            nonce = AuthController.decrypt(encryptedNonce, iv.value, key.value)
+        } else {
+            nonce = await AuthController.updateNonce(publicAddress)
+        }
+        return nonce
+    }
+
+    /**
+     * generate a new nonce and update the reference in the User
+     */
     static async updateNonce(publicAddress: string) {
         //generate nonce 
         const value = Math.floor(Math.random() * 99999999) + "";
@@ -131,26 +173,13 @@ export default class AuthController {
         return value
     }
 
-    static async getNonce(publicAddress: string) {
-        const users = await User.aggregate<TUserLookup>([{ $match: { publicAddress: publicAddress } }, { $lookup: { from: 'nonces', localField: 'nonceId', foreignField: '_id', as: 'nonces' } }]).toArray();
-        if (!users[0]) throw "User not found"
-        const user = users[0];
-
-
-        let nonce: string;
-        if (user.nonces.length == 0) {
-            nonce = await AuthController.updateNonce(publicAddress)
-        } else {
-            const encryptedNonce = user.nonces[0].value
-            const iv = await IV.findOne({ _id: user.nonces[0].ivId })
-            if (!iv) throw "IV not found"
-            const key = await Key.findOne({})
-            if(!key) throw "AES Secret missing"
-            nonce = AuthController.decrypt(encryptedNonce, iv.value, key.value)
-        }
-        return nonce
-    }
-
+    /**
+     * 
+     * @param plainData plainData to encode as string
+     * @param ivString InitialVector as hexstring
+     * @param keyString AES encryption key as hexstring
+     * @returns encrypted Data as string
+     */
     static encrypt(plainData: string, ivString: string, keyString: string) {
         const te = new TextEncoder();
         const iv = te.encode(ivString);
@@ -161,6 +190,13 @@ export default class AuthController {
         return encodeToString(bytecipher);
     }
 
+    /**
+     * 
+     * @param encryptedData encryptedData to decode as string
+     * @param ivString InitialVector as hexstring
+     * @param keyString AES encryption key as hexstring
+     * @returns plain data as string
+     */
     static decrypt(encryptedData: string, ivString: string, keyString: string) {
         const te = new TextEncoder();
         const td = new TextDecoder();
