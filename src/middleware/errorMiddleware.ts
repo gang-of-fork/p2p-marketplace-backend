b66be8bd84b49f4b79c85e6631da360600afe262
lumaghg <rotkehlchen567@gmail.com>
2022-10-05 20:33:09 +0200

    fixed header logging

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, OpineRequest, OpineResponse } from "../../deps.ts";
 import { PlantExchangeError } from "../types/errors.ts";
 
 export default function (err: PlantExchangeError, req: OpineRequest, res: OpineResponse, next: NextFunction) {
-    console.error(`Error ${err.statusCode}: ${err.msg}\nURL: ${req.originalUrl}\nBody: ${JSON.stringify(req.body, null, 4)}\nHeaders: ${JSON.stringify(req.headers, null, 4)}\n----------`)
+    console.error(`Error ${err.statusCode}: ${err.msg}\nURL: ${req.originalUrl}\nBody: ${JSON.stringify(req.body, null, 4)}\nHeaders: ${JSON.stringify({Authorization: req.headers.get("Authorization"), content_type: req.headers.get("Content-Type")}, null, 4)}\n----------`)
     if (err) {
         res.setStatus(err.statusCode).json({statusCode: err.statusCode, msg: err.msg || "Something went wrong"})
     }
